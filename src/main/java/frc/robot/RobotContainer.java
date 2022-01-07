4258d9ba9a2b6be6d6fb9af82a2136be26f7d456
AnthonyFurman <71467649+AnthonyFurman@users.noreply.github.com>
2022-01-06 18:59:42 -0800

    add arcade drive as default command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.GenericHID;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj.Joystick;
 import frc.robot.Constants.kJoy;
+import frc.robot.commands.ArcadeDrive;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -54,6 +55,8 @@ public class RobotContainer {
 
   
   public RobotContainer() {
+
+    m_drive.setDefaultCommand(new ArcadeDrive(m_drive));
 
     CameraServer.getInstance().startAutomaticCapture();
 
