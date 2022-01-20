363f605ddba8870a35ac96ce3edf7bddd390a107
PandaNinja10 <78180593+PandaNinja10@users.noreply.github.com>
2022-01-19 16:19:52 -0800

    it works sort of

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,9 +7,8 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
-
 /**
+ * 
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants.  This class should not be used for any other purpose.  All constants should be
  * declared globally (i.e. public static).  Do not put anything functional in this class.
@@ -19,52 +18,19 @@ import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
  */
 public class Constants {
 
-  public static final double kMaxVoltage = 12.0;
+    public static final double kMaxVoltage = 12.0;
 
-  public static final class JoyConstants {
-    public static final int kDriverJoy = 0;
-    public static final int kOperatorJoy = 1;
-    public static final double kJoystickDeadband = 0.07; // How much of joystick is "dead" zone [0,1]
-  }
+    public final class kJoy {
+        public static final int kDriverJoy = 0;
+        public static final int kOperatorJoy = 1;
+		public static final double kJoystickDeadband = 0.00; // How much of joystick is "dead" zone [0,1]
+    }
 
-  public static final class DriveConstants {
-    public static final int kLeftMotorPort = 1;
-    public static final int kLeftMotorPalPort = -1;
+    public final class kDrive {
+        public static final int kLeftMotorPort = 1;
+        // public static final int kLeftMotorPalPort = -1;
 
-    public static final int kRightMotorPort = 2;
-    public static final int kRightMotorPalPort = -1;
-
-    public static final double kTrackwidthMeters = 0.69;
-    public static final DifferentialDriveKinematics kDriveKinematics =
-        new DifferentialDriveKinematics(kTrackwidthMeters);
-  }
-
-  public static final class AutoConstants {
-
-    /** 
-     * Characterization data
-     */
-    // ksVolts -> adds +ksVolts or -ksVolts to overcome static friction in the direction of motion.
-    // kvVoltSecondsPerMeter -> Adds the values number of volts for every meter per second of velocity desired.
-    // kaVoltSecondsSquaredPerMeter -> Adds the values number of volts for every meter per second squared of acceleration desired.
-    public static final double ksVolts = 0.45633;
-    public static final double kvVoltSecondsPerMeter = 0.012947;
-    public static final double kaVoltSecondsSquaredPerMeter = 0.0035528;
-
-    // P Gain -> Number of ticks/100ms to apply for every ticks/100ms of error
-    public static final double kRamsetePGain = 0.019073;
-    //ARE THESE THE SAME??? IDK????
-    public static final double kPDriveVel = 0.019073;
-
-    /**
-     * Trajectories Data
-     */
-    public static final double kMaxSpeedMetersPerSecond = 3;
-    public static final double kMaxAccelerationMetersPerSecondSquared = 3;
-
-    // Reasonable baseline values for a RAMSETE follower in units of meters and seconds
-    // from WPI tutorial no clue what they mean
-    public static final double kRamseteB = 2;
-    public static final double kRamseteZeta = 0.7;
-  }
+        public static final int kRightMotorPort = 2;
+        // public static final int kRightMotorPalPort = -1;
+    }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,33 +7,18 @@
 
 package frc.robot;
 
+import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.GenericHID;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj.Joystick;
-import frc.robot.Constants.AutoConstants;
-import frc.robot.Constants.DriveConstants;
-import frc.robot.Constants.JoyConstants;
+import frc.robot.Constants.kJoy;
 import frc.robot.commands.ArcadeDrive;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
-
-import java.util.List;
-
 import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.RamseteController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
 
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -45,38 +30,38 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   public static Drivetrain m_drive = new Drivetrain();
 
-  static Joystick driver = new Joystick(JoyConstants.kDriverJoy);
-  static Joystick operator = new Joystick(JoyConstants.kOperatorJoy);
+  static Joystick driver = new Joystick(kJoy.kDriverJoy);
+  static Joystick operator = new Joystick(kJoy.kOperatorJoy);
 
   private static final JoystickButton driver_A = new JoystickButton(driver, 1),
-      driver_B = new JoystickButton(driver, 2), driver_X = new JoystickButton(driver, 3),
-      driver_Y = new JoystickButton(driver, 4), driver_LB = new JoystickButton(driver, 5),
-      driver_RB = new JoystickButton(driver, 6), driver_BACK = new JoystickButton(driver, 7),
-      driver_START = new JoystickButton(driver, 8);
+    driver_B = new JoystickButton(driver, 2), driver_X = new JoystickButton(driver, 3),
+    driver_Y = new JoystickButton(driver, 4), driver_LB = new JoystickButton(driver, 5),
+    driver_RB = new JoystickButton(driver, 6), driver_BACK = new JoystickButton(driver, 7),
+    driver_START = new JoystickButton(driver, 8);
 
   private static final JoystickButton operator_A = new JoystickButton(operator, 1),
-      operator_B = new JoystickButton(operator, 2), operator_X = new JoystickButton(operator, 3),
-      operator_Y = new JoystickButton(operator, 4), operator_LB = new JoystickButton(operator, 5),
-      operator_RB = new JoystickButton(operator, 6), operator_BACK = new JoystickButton(operator, 7),
-      operator_START = new JoystickButton(operator, 8);
+    operator_B = new JoystickButton(operator, 2), operator_X = new JoystickButton(operator, 3),
+    operator_Y = new JoystickButton(operator, 4), operator_LB = new JoystickButton(operator, 5),
+    operator_RB = new JoystickButton(operator, 6), operator_BACK = new JoystickButton(operator, 7),
+    operator_START = new JoystickButton(operator, 8);
 
   private static final POVButton driver_DPAD_UP = new POVButton(driver, 0),
-      driver_DPAD_RIGHT = new POVButton(driver, 90), driver_DPAD_DOWN = new POVButton(driver, 180),
-      driver_DPAD_LEFT = new POVButton(driver, 270);
+    driver_DPAD_RIGHT = new POVButton(driver, 90), driver_DPAD_DOWN = new POVButton(driver, 180),
+    driver_DPAD_LEFT = new POVButton(driver, 270);
 
   private static final POVButton operator_DPAD_UP = new POVButton(operator, 0),
-      operator_DPAD_RIGHT = new POVButton(driver, 90), operator_DPAD_DOWN = new POVButton(operator, 180),
-      operator_DPAD_LEFT = new POVButton(driver, 270);
+    operator_DPAD_RIGHT = new POVButton(driver, 90), operator_DPAD_DOWN = new POVButton(operator, 180),
+    operator_DPAD_LEFT = new POVButton(driver, 270);
 
   
   public RobotContainer() {
 
-   // m_drive.setDefaultCommand(new ArcadeDrive(m_drive));
+    m_drive.setDefaultCommand(new ArcadeDrive(m_drive));
 
     CameraServer.startAutomaticCapture();
 
-    m_drive.setDefaultCommand(
-        new RunCommand(() -> m_drive.arcadeDrive(getThrottleValue(), getTurnValue()), m_drive));
+    //m_drive.setDefaultCommand(
+    //  new RunCommand(() -> m_drive.arcadeDrive(getThrottleValue(), getTurnValue())));
     
     // Configure the button bindings
     configureButtonBindings();
@@ -98,61 +83,10 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    
-    // Create a voltage constraint to ensure we don't accelerate too fast
-    var autoVoltageConstraint =
-        new DifferentialDriveVoltageConstraint(
-            new SimpleMotorFeedforward(
-                AutoConstants.ksVolts,
-                AutoConstants.kvVoltSecondsPerMeter,
-                AutoConstants.kaVoltSecondsSquaredPerMeter),
-            DriveConstants.kDriveKinematics,
-            10);
-
-    // Create config for trajectory
-    TrajectoryConfig config =
-        new TrajectoryConfig(
-                AutoConstants.kMaxSpeedMetersPerSecond,
-                AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-            // Add kinematics to ensure max speed is actually obeyed
-            .setKinematics(DriveConstants.kDriveKinematics)
-            // Apply the voltage constraint
-            .addConstraint(autoVoltageConstraint);
-
-    // An example trajectory to follow.  All units in meters.
-    Trajectory exampleTrajectory =
-        TrajectoryGenerator.generateTrajectory(
-            // Start at the origin facing the +X direction
-            new Pose2d(0, 0, new Rotation2d(0)),
-            // Pass through these two interior waypoints, making an 's' curve path
-            List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-            // End 3 meters straight ahead of where we started, facing forward
-            new Pose2d(3, 0, new Rotation2d(0)),
-            // Pass config
-            config);
-
-    RamseteCommand ramseteCommand =
-        new RamseteCommand(
-            exampleTrajectory,
-            m_drive::getPose,
-            new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
-            new SimpleMotorFeedforward(
-                AutoConstants.ksVolts,
-                AutoConstants.kvVoltSecondsPerMeter,
-                AutoConstants.kaVoltSecondsSquaredPerMeter),
-            DriveConstants.kDriveKinematics,
-            m_drive::getWheelSpeeds,
-            new PIDController(AutoConstants.kPDriveVel, 0, 0),
-            new PIDController(AutoConstants.kPDriveVel, 0, 0),
-            // RamseteCommand passes volts to the callback
-            m_drive::tankDriveVolts,
-            m_drive);
-
-    // Reset odometry to the starting pose of the trajectory.
-    m_drive.resetOdometry(exampleTrajectory.getInitialPose());
-
-    // Run path following command, then stop at the end.
-    return ramseteCommand.andThen(() -> m_drive.tankDriveVolts(0, 0));
+    // An spin in circle
+    return new SequentialCommandGroup(
+      new RunCommand(() -> m_drive.tankDrive(0.2, -0.2))
+      );
   }
 
   /**
@@ -164,7 +98,7 @@ public class RobotContainer {
     // Controllers y-axes are natively up-negative, down-positive. This method
     // corrects that by returning the opposite of the y-value
     // 1 represents up/down axis on the left joystick
-    return -deadbandX(driver.getRawAxis(1), JoyConstants.kJoystickDeadband);
+    return driver.getRawAxis(1);
   }
 
   /**
@@ -173,8 +107,8 @@ public class RobotContainer {
    * @return the deadbanded turn input from the main driver controller
    */
   public static double getTurnValue() {
-    // 5 represents left/right axis on the right joystick
-    return deadbandX(driver.getRawAxis(5), JoyConstants.kJoystickDeadband);
+      // 5 represents left/right axis on the right joystick
+      return driver.getRawAxis(4);
   }
 
   /**
@@ -187,11 +121,11 @@ public class RobotContainer {
    */
   public static double deadbandX(double input, double deadband) {
     if (Math.abs(input) <= deadband) {
-      return 0;
+        return 0;
     } else if (Math.abs(input) == 1) {
-      return input;
+        return input;
     } else {
-      return (1 / (1 - deadband) * (input + Math.signum(-input) * deadband));
+        return (1 / (1 - deadband) * (input + Math.signum(-input) * deadband));
     }
   }
 }
