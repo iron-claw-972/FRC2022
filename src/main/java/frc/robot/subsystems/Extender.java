79c2ea12097f4c5a5bae2b190618c8af3c753b8d
OctraSource <92061729+JusticePeyton@users.noreply.github.com>
2022-01-24 16:40:44 -0800

    inverter implimentation

--- src/main/java/frc/robot/subsystems/Extender.java
+++ src/main/java/frc/robot/subsystems/Extender.java
@@ -9,12 +9,17 @@ import frc.robot.Constants.ExtenderConstants;
 public class Extender {
     private final CANSparkMax m_lmotor = ControllerFactory.createSparkMAX(ExtenderConstants.kLeftExtenderPort, MotorType.kBrushless);
     private final CANSparkMax m_rmotor = ControllerFactory.createSparkMAX(ExtenderConstants.kRightExtenderPort, MotorType.kBrushless);
+    private int inverter = 1;
+
+    public void inverter() {
+        inverter *= -1;
+    }
 
     public void runLeft(double pow) {
-        m_lmotor.set(pow);
+        m_lmotor.set(pow * inverter);
     }
 
     public void runRight(double pow) {
-        m_rmotor.set(pow);
+        m_rmotor.set(pow * inverter);
     }
 }
