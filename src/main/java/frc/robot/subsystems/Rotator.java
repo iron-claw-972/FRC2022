f6d6ffcdc62ce3466a654caa486e7e446a25ff3c
OctraSource <92061729+JusticePeyton@users.noreply.github.com>
2022-01-24 16:41:23 -0800

    inverter implementation

--- src/main/java/frc/robot/subsystems/Rotator.java
+++ src/main/java/frc/robot/subsystems/Rotator.java
@@ -9,12 +9,17 @@ import frc.robot.Constants.RotatorConstants;
 public class Rotator {
     private final CANSparkMax m_lmotor = ControllerFactory.createSparkMAX(RotatorConstants.kLeftRotatorPort, MotorType.kBrushless);
     private final CANSparkMax m_rmotor = ControllerFactory.createSparkMAX(RotatorConstants.kRightRotatorPort, MotorType.kBrushless);
+    private int inverter = 1;
+
+    public void inverter() {
+        inverter *= -1;
+    }
 
     public void runLeft(double pow) {
-        m_lmotor.set(pow);
+        m_lmotor.set(pow * inverter);
     }
 
     public void runRight(double pow) {
-        m_rmotor.set(pow);
+        m_rmotor.set(pow * inverter);
     }
 }
