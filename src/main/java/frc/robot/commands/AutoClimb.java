3acfe1402ac527ce499416e88c101b6faf5e25a3
RobeVale <valr5254@lgsstudent.org>
2022-01-28 15:12:03 -0800

    Auto Climb Task List

---
+++ src/main/java/frc/robot/commands/AutoClimb.java
@@ -0,0 +1,19 @@
+package frc.robot.commands;
+
+public class AutoClimb {
+    /*
+    NOTE: NONE OF THIS IS FINAL UNTIL CLIMBER DESIGN IS FINALIZED
+
+    In summary, autonomize:
+    Rotate moving hooks very slightly away from bar
+    Extend moving hooks
+    Rotate moving hooks onto bar
+    Rotate stationary hooks away from bar
+    Rotate stationary hooks onto bar
+    Rotate to bar to put stationary hooks on
+    Rotate moving hooks 47 degrees
+    Extend moving hooks
+    Rotate moving hooks back
+    Compress moving hooks (Might not be needed)
+    */
+}
