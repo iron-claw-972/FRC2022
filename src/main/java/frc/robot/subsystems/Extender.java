c9070f3c4ee28dea2f097126f21600cd03a8abdc
OctraSource <92061729+JusticePeyton@users.noreply.github.com>
2022-01-29 15:09:18 -0800

    hopefully working extender with limits
    it built
    yaaay

--- src/main/java/frc/robot/subsystems/Extender.java
+++ src/main/java/frc/robot/subsystems/Extender.java
@@ -1,26 +1,45 @@
 package frc.robot.subsystems;
 
+
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
-import ctre_shims.TalonEncoder;
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.ControllerFactory;
-import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.ExtenderConstants;
 
 
 public class Extender {
-    //TODO: check if the extender motors are talonfx motors
     private final WPI_TalonFX m_leftMotor = ControllerFactory.createTalonFX(ExtenderConstants.kLeftExtenderPort);
     private final WPI_TalonFX m_rightMotor = ControllerFactory.createTalonFX(ExtenderConstants.kRightExtenderPort);
+    
+    public Extender() {
+        m_leftMotor.follow(m_rightMotor);
 
-    private final TalonEncoder m_leftEncoder = new TalonEncoder(m_leftMotor, DriveConstants.kLeftEncoderReversed);
-    private final TalonEncoder m_rightEncoder = new TalonEncoder(m_rightMotor, DriveConstants.kRightEncoderReversed);
+        // so that the motors spin in the same direction
+        m_leftMotor.setInverted(true);
+        m_rightMotor.setInverted(false);
 
-    private boolean disabler = false;
+        // so that encoder values aren't negative
+        m_rightMotor.setSensorPhase(true);
+        m_leftMotor.setSensorPhase(false);
 
-    public void run(double pow) {
-        m_leftMotor.set(pow);
-        m_rightMotor.set(-pow);
+        // the lowest tick limit is 0, and must be checked every 10 milliseconds
+        m_leftMotor.configReverseSoftLimitThreshold(0, 10);
+        m_rightMotor.configReverseSoftLimitThreshold(0, 10);
+
+        // converts the length of the arm in feet to ticks and makes that the maximum tick limit, it's checked every 10 milliseconds
+        m_leftMotor.configForwardSoftLimitThreshold(ExtenderConstants.kExtenderMaxArmLength / ExtenderConstants.kExtenderTickMultiple, 10);
+        m_rightMotor.configForwardSoftLimitThreshold(ExtenderConstants.kExtenderMaxArmLength / ExtenderConstants.kExtenderTickMultiple, 10);
+
+        // every time the robot is started, it MUST start at maximum compression in order to maintain consistency
+        m_leftMotor.setSelectedSensorPosition(0.0);
+        m_rightMotor.setSelectedSensorPosition(0.0);
     }
-}
+
+    // called in RobotContainer by configureButtonBindings()
+    public void run(double pow) {
+        m_rightMotor.set(pow);
+        // a pop-up in shuffleboard that allows you to see how much the arm extended in feet
+        SmartDashboard.putNumber("Extended in Feet", m_rightMotor.getSelectedSensorPosition() * ExtenderConstants.kExtenderTickMultiple);
+    }
+} 
