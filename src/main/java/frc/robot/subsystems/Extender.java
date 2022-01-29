1686585a5f9150ec92c35bf8590ba9059705d337
OctraSource <92061729+JusticePeyton@users.noreply.github.com>
2022-01-29 12:03:24 -0800

    autonomous prep

--- src/main/java/frc/robot/subsystems/Extender.java
+++ src/main/java/frc/robot/subsystems/Extender.java
@@ -2,16 +2,25 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import ctre_shims.TalonEncoder;
+
 import frc.robot.ControllerFactory;
+import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.ExtenderConstants;
+
 
 public class Extender {
     //TODO: check if the extender motors are talonfx motors
-    private final WPI_TalonFX m_lmotor = ControllerFactory.createTalonFX(ExtenderConstants.kLeftExtenderPort);
-    private final WPI_TalonFX m_rmotor = ControllerFactory.createTalonFX(ExtenderConstants.kRightExtenderPort);
+    private final WPI_TalonFX m_leftMotor = ControllerFactory.createTalonFX(ExtenderConstants.kLeftExtenderPort);
+    private final WPI_TalonFX m_rightMotor = ControllerFactory.createTalonFX(ExtenderConstants.kRightExtenderPort);
+
+    private final TalonEncoder m_leftEncoder = new TalonEncoder(m_leftMotor, DriveConstants.kLeftEncoderReversed);
+    private final TalonEncoder m_rightEncoder = new TalonEncoder(m_rightMotor, DriveConstants.kRightEncoderReversed);
+
+    private boolean disabler = false;
 
     public void run(double pow) {
-        m_lmotor.set(pow);
-        m_rmotor.set(-pow);
+        m_leftMotor.set(pow);
+        m_rightMotor.set(-pow);
     }
 }
