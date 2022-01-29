b4964f43cab04d7a2642f25589eb9738550a2c13
OctraSource <92061729+JusticePeyton@users.noreply.github.com>
2022-01-29 12:09:35 -0800

    imported encoder

--- src/main/java/frc/robot/subsystems/Rotator.java
+++ src/main/java/frc/robot/subsystems/Rotator.java
@@ -2,17 +2,23 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.Encoder;
 
 import frc.robot.ControllerFactory;
 import frc.robot.Constants.RotatorConstants;
 
+
 public class Rotator {
     //TODO: check if whatever is connected to the sparkmax is brush/brushless
-    private final CANSparkMax m_lmotor = ControllerFactory.createSparkMAX(RotatorConstants.kLeftRotatorPort, MotorType.kBrushless);
-    private final CANSparkMax m_rmotor = ControllerFactory.createSparkMAX(RotatorConstants.kRightRotatorPort, MotorType.kBrushless);
+    private final CANSparkMax m_leftMotor = ControllerFactory.createSparkMAX(RotatorConstants.kLeftRotatorPort, MotorType.kBrushless);
+    private final CANSparkMax m_rightMotor = ControllerFactory.createSparkMAX(RotatorConstants.kRightRotatorPort, MotorType.kBrushless);
     
+    Encoder m_leftEncoder = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
+    Encoder m_rightEncoder = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
+
     public void run(double pow) {
-        m_lmotor.set(pow);
-        m_rmotor.set(-pow);
+        // runs but doesn't stop running even if it's at the design limit
+        m_leftMotor.set(pow);
+        m_rightMotor.set(-pow);
     }
 }
