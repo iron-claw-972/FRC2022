d09337e1f1b2437599a824ab7de979ca1b1b78a6
AnthonyFurman <71467649+AnthonyFurman@users.noreply.github.com>
2022-01-29 13:52:50 -0800

    imports

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -15,8 +15,6 @@ import edu.wpi.first.math.filter.SlewRateLimiter;
 import frc.robot.controls.Driver;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.*;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.fasterxml.jackson.annotation.JsonCreator.Mode;
 import com.kauailabs.navx.frc.AHRS;
