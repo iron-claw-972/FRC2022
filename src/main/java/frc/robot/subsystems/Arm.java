d480586157ba004d8e33197fbcb9061a835e7edf
PandaNinja10 <78180593+PandaNinja10@users.noreply.github.com>
2022-01-31 17:47:08 -0800

    PID added

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -6,17 +6,84 @@ import ctre_shims.PhoenixMotorControllerGroup;
 import ctre_shims.TalonEncoder;
 import frc.robot.ControllerFactory;
 import frc.robot.Constants.ArmConstants;
-import frc.robot.Constants.DriveConstants;
 
 public class Arm {
+  //set to defult radian angle probly should set to
+  double armGoal = 0;
+  
   private final WPI_TalonFX m_leftMotor  = ControllerFactory.createTalonFX(ArmConstants.kLeftMotorPort);
   private final WPI_TalonFX m_rightMotor = ControllerFactory.createTalonFX(ArmConstants.kRightMotorPort);
+  
 
-    private final PhoenixMotorControllerGroup m_motors = new PhoenixMotorControllerGroup(m_leftMotor, m_rightMotor);
+  private final PhoenixMotorControllerGroup m_motors = new PhoenixMotorControllerGroup(m_leftMotor, m_rightMotor);
+  
+  // arm encoder
+  private final TalonEncoder m_leftEncoder = new TalonEncoder(m_leftMotor);
+  private final TalonEncoder m_rightEncoder = new TalonEncoder(m_rightMotor);
+
+  public Arm(){
+    m_rightMotor.setInverted(true);
+  }
+  public double getPosition(){
+    // System.out.println(m_leftEncoder.getDistance();
+    return m_leftEncoder.getDistance() * ArmConstants.kEncoderRadiansPerPulse;
+  }
+
+  public double getVelocity(){
+      // System.out.println(m_leftEncoder.getRate());
+      return m_leftEncoder.getRate();
+  }
+
+  public void moveGoal(double target){
+    armGoal = armGoal + target;
+  }
+
+  public double getGoal(){
+      return armGoal;
+  }
+  
+  public void setEncodersRadians(double encoderVal){
+
+  }
+
+
+  double maxOutput = 0.1;
+  double maxError = 2;
+  double error;
+  double derivative;
+  double errorPrior;
+  double pGain = 0.1;
+  double iGain = 0.1;
+  double dGain = 0.1;
+
+  double outPutVal;
+  double integralPrior;
+  double integral =0;
+  double iterationTime = 1;
+
+  public void posPID() {
+
+    error = armGoal - getPosition();
+    integral = integralPrior + error * iterationTime;
+    derivative = (error - errorPrior) / iterationTime;
+
+    outPutVal = pGain *error + iGain *integral + dGain *derivative;
+    outPutVal = outPutVal * Math.cos(getPosition());
     
-    // arm encoder
-    private final TalonEncoder m_leftEncoder = new TalonEncoder(m_leftMotor, DriveConstants.kLeftEncoderReversed);
-    private final TalonEncoder m_rightEncoder = new TalonEncoder(m_rightMotor, DriveConstants.kRightEncoderReversed);
+    errorPrior = error;
+    integralPrior = integral;
 
+    /*
+    if (outPutVal > maxOutput){
+      outPutVal = maxOutput;
+    }if (outPutVal < -maxOutput) {
+      outPutVal = -maxOutput;
+    }if (maxError > error && error > maxError) {
+      outPutVal = 0;
+    }
+    */
+
+    m_motors.set(outPutVal);
+  }
     
 }
