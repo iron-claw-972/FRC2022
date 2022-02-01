060a9065981ff422a64ed9f018472777d7c407ed
omega <62168528+OmegaTWC@users.noreply.github.com>
2022-01-31 16:01:25 -0800

    Constants!

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -15,15 +15,10 @@ import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.util.Color;
 
 public class Shooter extends SubsystemBase {
-<<<<<<< HEAD
-    private final WPI_TalonFX m_motor = ControllerFactory.createTalonFX(ShooterConstants.kShooterMotorPort);
-
-=======
     
->>>>>>> e25ec55ae945e0ada692c874cf3f4b210ce8d4e9
     public Shooter() {}
 
-    private final WPI_TalonFX m_motor = ControllerFactory.createTalonFX(1);
+    private final WPI_TalonFX m_motor = ControllerFactory.createTalonFX(ShooterConstants.kShooterMotorPort);
     private final PIDController shooterPID = new PIDController(ShooterConstants.kShooterP, ShooterConstants.kShooterI, ShooterConstants.kShooterD);
 
     @Override
@@ -32,29 +27,6 @@ public class Shooter extends SubsystemBase {
     }
 
     public void setSpeed(double speed) {
-<<<<<<< HEAD
-        m_motor.set(ControlMode.PercentOutput, speed);
-
-    }
-
-    public void setBackOutakeSpeed() {
-        setSpeed(ShooterConstants.speed);
-
-    }
-
-    public void setFrontkOutakeSpeed() {
-        setSpeed(ShooterConstants.speed);
-
-    }
-
-    public void setBackOutakeFarSpeed() {
-        setSpeed(ShooterConstants.speed* ShooterConstants.multiplier);
-
-    }
-
-    public void setFrontOutakeFarSpeed() {
-        setSpeed(ShooterConstants.speed* ShooterConstants.multiplier);
-=======
         m_motor.set(ControlMode.PercentOutput, shooterPID.calculate(speed));
     }
 
@@ -73,14 +45,9 @@ public class Shooter extends SubsystemBase {
     public void setFrontOutakeFarSpeed() {
         setSpeed(ShooterConstants.kFrontOutakeSpeed * ShooterConstants.kFarMultiplier);
     }
->>>>>>> e25ec55ae945e0ada692c874cf3f4b210ce8d4e9
 
     public void setBackOutakeFarSpeed() {
         setSpeed(ShooterConstants.kBackOutakeSpeed * ShooterConstants.kFarMultiplier);
-    }
-
-    public void intake(){
-        setSpeed(ShooterConstants.speed*-1);
     }
 
     public void stop() {
@@ -90,24 +57,11 @@ public class Shooter extends SubsystemBase {
     private final I2C.Port i2cPort = I2C.Port.kOnboard;
     private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
 
-<<<<<<< HEAD
-    public void robotInit() {
-        m_colorMatcher.addColorMatch(Color.kFirstBlue);
-        m_colorMatcher.addColorMatch(Color.kFirstRed);
-    }
-    
-    public String isColor(double r, double g, double b){
-        if (b> 0.4) {
-          return "blue";
-        } else if (r > 0.4) {
-          return "red";
-=======
     public String isColor(double red, double green, double blue) {
         if (blue > 0.4) {
             return "blue";
         } else if (red > 0.4) {
             return "red";
->>>>>>> e25ec55ae945e0ada692c874cf3f4b210ce8d4e9
         }
         return "none";
     }
@@ -115,13 +69,6 @@ public class Shooter extends SubsystemBase {
     public String ballColor() {
         Color detectedColor = m_colorSensor.getColor();
         return isColor(detectedColor.red, detectedColor.green, detectedColor.blue);
-<<<<<<< HEAD
-
-    }
-
-    
-
-=======
     }
 
     public Boolean containsBall() {
@@ -133,7 +80,6 @@ public class Shooter extends SubsystemBase {
         System.out.println("No object detected, proximity: " + ballProximity);
         return false;
     }
->>>>>>> e25ec55ae945e0ada692c874cf3f4b210ce8d4e9
 
     public Boolean reachedSetpoint(double targetSpeed) {
         // returns true if the targetSpeed = current motor speed
