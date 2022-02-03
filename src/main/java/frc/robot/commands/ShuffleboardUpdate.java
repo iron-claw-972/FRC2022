d4048f5058edb7aba04f0f8507cb4bb69a27c442
RobeVale <valr5254@lgsstudent.org>
2022-02-02 17:10:13 -0800

    yee

--- src/main/java/frc/robot/commands/ShuffleboardUpdate.java
+++ src/main/java/frc/robot/commands/ShuffleboardUpdate.java
@@ -5,12 +5,12 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.controls.Driver;
 
 public class ShuffleboardUpdate extends CommandBase {
-    @Override
-    public void execute() {
+  @Override
+  public void execute() {
 
-      //drive mode
-      SmartDashboard.putString("Drive Mode", Driver.getDriveMode().toString());
+    //drive mode
+    SmartDashboard.putString("Drive Mode", Driver.getDriveMode().toString());
 
-    }
   }
+}
   
