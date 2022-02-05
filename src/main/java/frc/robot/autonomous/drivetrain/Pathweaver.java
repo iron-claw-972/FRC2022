b4beba26a85d287a5b0719a3af412a91621bad14
AnthonyFurman <71467649+AnthonyFurman@users.noreply.github.com>
2022-02-05 15:18:35 -0800

    fix pathweaver

--- src/main/java/frc/robot/autonomous/drivetrain/Pathweaver.java
+++ src/main/java/frc/robot/autonomous/drivetrain/Pathweaver.java
@@ -26,64 +26,64 @@ public class Pathweaver {
   private static RamseteCommand ramseteCommand;
 
   public static void setupAutonomousTrajectory(String trajectoryName) {
-        
-  m_drive.resetOdometry(autonomousTrajectory.getInitialPose());
 
-  String trajectoryJSON = "paths/output/" + trajectoryName + ".wpilib.json";
-  try {
-    Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
-    autonomousTrajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
-  } catch (IOException ex) {
-    DriverStation.reportWarning(
-    "Unable to open trajectory: " + trajectoryJSON + "\n" +
-    "Falling back to default trajectory",
-    ex.getStackTrace());
-    
-    // Create a voltage constraint to ensure we don't accelerate too fast
-    var autoVoltageConstraint = new DifferentialDriveVoltageConstraint(
-      m_drive.getFeedforward(),
-      m_drive.getDriveKinematics(),
-      Constants.kMaxVoltage);
-    
-    // Create config for trajectory
-    TrajectoryConfig config = new TrajectoryConfig(
-      AutoConstants.kMaxSpeedMetersPerSecond,
-      AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-        // Add kinematics to ensure max speed is actually obeyed
-        .setKinematics(m_drive.getDriveKinematics())
-        // Apply the voltage constraint
-        .addConstraint(autoVoltageConstraint);
-              
+    String trajectoryJSON = "paths/output/" + trajectoryName + ".wpilib.json";
+    try {
+      Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+      autonomousTrajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+    } catch (IOException ex) {
+      DriverStation.reportWarning(
+          "Unable to open trajectory: " + trajectoryJSON + "\n" +
+              "Falling back to default trajectory",
+          ex.getStackTrace());
+
+      // Create a voltage constraint to ensure we don't accelerate too fast
+      var autoVoltageConstraint = new DifferentialDriveVoltageConstraint(
+          m_drive.getFeedforward(),
+          m_drive.getDriveKinematics(),
+          Constants.kMaxVoltage);
+
+      // Create config for trajectory
+      TrajectoryConfig config = new TrajectoryConfig(
+          AutoConstants.kMaxSpeedMetersPerSecond,
+          AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+              // Add kinematics to ensure max speed is actually obeyed
+              .setKinematics(m_drive.getDriveKinematics())
+              // Apply the voltage constraint
+              .addConstraint(autoVoltageConstraint);
+
       // Fallback to default trajectory
       autonomousTrajectory = TrajectoryGenerator.generateTrajectory(
-        // Make a square and end where we started
-        new Pose2d(0, 0, new Rotation2d(0)),
-        List.of(new Translation2d(3, 0), new Translation2d(3, 3), new Translation2d(0, 3)),
-        new Pose2d(0, 0, new Rotation2d(0)),
-        // Pass config
-        config);
+          // Make a square and end where we started
+          new Pose2d(0, 0, new Rotation2d(0)),
+          List.of(new Translation2d(3, 0), new Translation2d(3, 3), new Translation2d(0, 3)),
+          new Pose2d(0, 0, new Rotation2d(0)),
+          // Pass config
+          config);
+    }
+
+    ramseteCommand = new RamseteCommand(
+        autonomousTrajectory,
+        m_drive::getPose,
+        m_drive.getRamseteController(),
+        m_drive.getFeedforward(),
+        m_drive.getDriveKinematics(),
+        m_drive::getWheelSpeeds,
+        m_drive.getLeftRamsetePIDController(),
+        m_drive.getRightRamsetePIDController(),
+        // RamseteCommand passes volts to the callback
+        m_drive::tankDriveVolts,
+        m_drive);
+
+    m_drive.resetOdometry(autonomousTrajectory.getInitialPose());
   }
 
-  ramseteCommand = new RamseteCommand(
-    autonomousTrajectory,
-    m_drive::getPose,
-    m_drive.getRamseteController(),
-    m_drive.getFeedforward(),
-    m_drive.getDriveKinematics(),
-    m_drive::getWheelSpeeds,
-    m_drive.getLeftRamsetePIDController(),
-    m_drive.getRightRamsetePIDController(),
-    // RamseteCommand passes volts to the callback
-    m_drive::tankDriveVolts,
-    m_drive);
-  }
-    
-  //returns auto command group
-  public static Command pathweaverCommand(){
+  // returns auto command group
+  public static Command pathweaverCommand() {
     // Run path following command, then stop at the end. At the same time intake.
-    // "Deadline" is the first command, 
+    // "Deadline" is the first command,
     // meaning the whole group will stop once the first command does.
     return new ParallelDeadlineGroup(
-    ramseteCommand.andThen(() -> m_drive.tankDriveVolts(0, 0)));
+        ramseteCommand.andThen(() -> m_drive.tankDriveVolts(0, 0)));
   }
-} 
+}
