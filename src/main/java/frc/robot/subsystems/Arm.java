ec4e15ca2da385d535cbaf9265e6828e029a5f1b
RobeVale <valr5254@lgsstudent.org>
2022-02-06 14:37:02 -0800

    Half Finished

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -4,24 +4,72 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import ctre_shims.PhoenixMotorControllerGroup;
 import ctre_shims.TalonEncoder;
+import ctre_shims.TalonEncoderSim;
 import frc.robot.ControllerFactory;
 import frc.robot.Constants.ArmConstants;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Arm {
-  //set to defult radian angle probly should set to
-  double armGoal = 0;
-  
-  private final WPI_TalonFX m_leftMotor  = ControllerFactory.createTalonFX(ArmConstants.kLeftMotorPort);
-  private final WPI_TalonFX m_rightMotor = ControllerFactory.createTalonFX(ArmConstants.kRightMotorPort);
-  
+public class Arm extends SubsystemBase{
+  private final WPI_TalonFX m_motor;
+  private final TalonEncoder m_encoder;
+  private double setpoint = ArmConstants.kRotatorSetpoint;
 
-  private final PhoenixMotorControllerGroup m_motors = new PhoenixMotorControllerGroup(m_leftMotor, m_rightMotor);
-  
-  // arm encoder
-  private final TalonEncoder m_leftEncoder = new TalonEncoder(m_leftMotor);
-  private final TalonEncoder m_rightEncoder = new TalonEncoder(m_rightMotor);
-  
+  public Arm(int port, boolean left) {
+    m_motor = ControllerFactory.createTalonFX(port);
+    m_encoder= new TalonEncoder(m_motor);
 
+    if (left) {
+      // so that encoder values aren't negative
+      m_motor.setSensorPhase(true);
+      // so that the arm doesn't spin in an opposing direction
+      m_motor.setInverted(true);
+    }
+
+    // the lowest tick limit is 0, and must be checked every 10 milliseconds
+    m_motor.configReverseSoftLimitThreshold(0, 10);
+
+    // converts the length of the arm in inches to ticks and makes that the maximum tick limit, it's checked every 10 milliseconds
+    m_motor.configForwardSoftLimitThreshold(ArmConstants.kRotatorMaxArmTicks, 10);
+
+    // every time the robot is started, arm MUST start at maximum compression in order to maintain consistency
+    // TODO: Make this better.
+    m_encoder.reset();
+
+    // so that the limiters are enabled
+    m_motor.configForwardSoftLimitEnable(true);
+    m_motor.configReverseSoftLimitEnable(true);
+  }
+
+  public boolean reachedSetpoint() {
+    // if the current tick position is within the setpoint's range (setpoint +- 10), return true, otherwise return false
+    return(m_encoder.getDistance() >= (setpoint / ArmConstants.kRotatorTickMultiple) - ArmConstants.kRotatorTolerance
+    && m_encoder.getDistance() <= (setpoint / ArmConstants.kRotatorTickMultiple) + ArmConstants.kRotatorTolerance);
+  }
+
+  // called in RobotContainer by button binds
+  public void set(double distance){
+    setpoint = distance;
+  }
+
+
+  @Override
+  public void periodic(){
+    if (reachedSetpoint() == false) {
+      // sets the motor to go to a setpoint
+      // the setpoint is tick value
+      m_motor.set(ArmConstants.rotatorPID.calculate(m_encoder.getDistance(), setpoint));
+
+      // a pop-up in shuffleboard that allows you to see how much the arm extended in inches
+      SmartDashboard.putNumber("Current Extension (Inches)", m_encoder.getDistance() * ArmConstants.kRotatorTickMultiple);
+
+      // so we know the value
+      // System.out.println(setpoint);
+      System.out.println(m_encoder.getDistance());
+    }
+  }
+  /*
+  //Michael's old code that may or may not need to be reused
   public Arm(){
     m_rightMotor.setInverted(true);
   }
@@ -60,11 +108,10 @@ public class Arm {
     }if (maxError > error && error > maxError) {
       outPutVal = 0;
     }
-    */
 
     m_motors.set(outPutVal);
   }
-  
+
   public double getPosition(){
     // System.out.println(m_leftEncoder.getDistance();
     return m_leftEncoder.getDistance() * ArmConstants.kEncoderRadiansPerPulse;
@@ -91,6 +138,5 @@ public class Arm {
   public boolean atGoal(){
     return (error < errorPrior && errorPrior < maxError);
   }
-  
-    
+  */
 }
