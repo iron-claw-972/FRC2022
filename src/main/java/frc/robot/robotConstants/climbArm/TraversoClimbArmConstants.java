817d637e58ec5d479649015bfa8aaaf0db68cabb
OctraSource <92061729+JusticePeyton@users.noreply.github.com>
2022-02-13 15:07:15 -0800

    syntax fixes
    "final static" sounds terrible in comparison to "static final"

--- src/main/java/frc/robot/robotConstants/climbArm/TraversoClimbArmConstants.java
+++ src/main/java/frc/robot/robotConstants/climbArm/TraversoClimbArmConstants.java
@@ -2,32 +2,33 @@ package frc.robot.robotConstants.climbArm;
 
 public class TraversoClimbArmConstants {
     // the duty cycle encoder ports of the arm
-    public final static int kArmRightEncoder = 1;
-    public final static int kArmLeftEncoder = 0;
+    public static final int kArmRightEncoder = 1;
+    public static final int kArmLeftEncoder = 0;
 
     // motor clamping
-    public final static double kMotorClamp = 0.3;
+    public static final double kMotorClamp = 0.3;
 
     // the motor ports of the arm
-    public final static int kArmRightMotor = -1;
-    public final static int kArmLeftMotor = 24;
+    public static final int kArmRightMotor = -1;
+    public static final int kArmLeftMotor = 24;
 
-    public final static double kArmMaxDegree = 30;
+    public static final double kArmMaxDegree = 30;
 
     // Current Tick Value * Degree Multiple = Current Angle
-    public final static double kArmDegreeMultiple = 360.0;
-    public final static double kArmZeroEncoderDegrees = 0;
+    public static final double kArmDegreeMultiple = 360.0;
+    public static final double kArmZeroEncoderDegrees = 0;
 
     // the distance allowed from the setpoint (IN DECIMAL DEGREES (1 = 360, .5 = 180, .25 = 90))
-    public final static double kArmTolerance = 3;
+    public static final double kArmTolerance = 3;
 
     // whether the gearbox is flipped
-    public final static int kFlipped = 1;
+    public static final int kFlipped = 1;
 
-    public final static double kArmMaxDegreeTicks = kArmMaxDegree / kArmDegreeMultiple;
+    public static final double kArmMaxDegreeTicks = kArmMaxDegree / kArmDegreeMultiple;
     
     // locations
-    public final static double kNinetyDeg = 90;
-    public final static double kMaxBackward = 135;
-    public final static double kMaxForward = 80;
+    public static final double kNinetyDeg = 90;
+    public static final double kMaxBackward = 135;
+    public static final double kMaxForward = 80;
+    public static final double kToBar = 110;
 }
--- src/main/java/frc/robot/robotConstants/extenderArm/TraversoExtenderArmConstants.java
+++ src/main/java/frc/robot/robotConstants/extenderArm/TraversoExtenderArmConstants.java
@@ -3,31 +3,31 @@ package frc.robot.robotConstants.extenderArm;
 public class TraversoExtenderArmConstants {
   // extending motors (for climber)
   // TODO: Change these ports!
-  public final static int kRightExtenderPort = 2;
-  public final static int kLeftExtenderPort = 1;
+  public static final int kRightExtenderPort = 2;
+  public static final int kLeftExtenderPort = 1;
 
   // the arm's max height - the arm's lowest height (max extension - max compression);
   // TODO: Check the robot's arm length!
-  public final static double kExtenderMaxArmLength = 63 - 38; // at its highest, it's 63 inches, at its lowest its 38 inches
+  public static final double kExtenderMaxArmLength = 63 - 38; // at its highest, it's 63 inches, at its lowest its 38 inches
 
   // used to convert ticks to inches
-  public final static double kExtenderTicksPerRotation = 1/2048; // every rotation is 2048 ticks
-  public final static double kExtenderGearRatio = 1/20; // gear ration of 20:1
-  public final static double kExtenderInchesPerRotation = (Math.PI * 1) / 1; // 1 pi inches per rotation
+  public static final double kExtenderTicksPerRotation = 1/2048; // every rotation is 2048 ticks
+  public static final double kExtenderGearRatio = 1/20; // gear ration of 20:1
+  public static final double kExtenderInchesPerRotation = (Math.PI * 1) / 1; // 1 pi inches per rotation
 
   // Ticks Per Rotation * Gear Ratio * Inches Per Rotation = Tick Multiple
   // Results as 0 when you calculate this in WPIlib, but it's definitely not 0
-  public final static double kExtenderTickMultiple =  0.00007669903; //(kExtenderTicksPerRotation * kExtenderGearRatio * kExtenderInchesPerRotation);
+  public static final double kExtenderTickMultiple =  0.00007669903; //(kExtenderTicksPerRotation * kExtenderGearRatio * kExtenderInchesPerRotation);
 
   // the arm's length in ticks
-  public final static double kExtenderMaxArmTicks = kExtenderMaxArmLength / kExtenderTickMultiple;
+  public static final double kExtenderMaxArmTicks = kExtenderMaxArmLength / kExtenderTickMultiple;
 
   // motor clamps
-  public final static double kMotorClamp = .1;
+  public static final double kMotorClamp = .1;
   
   // locations
-  public final static double kMaxUpwards = kExtenderMaxArmLength;
-  public final static double kMaxDownwards = 0;
-  public final static double kHalfway = kExtenderMaxArmLength / 2;
-  public final static double kSlightlyUpward = 6;
+  public static final double kMaxUpwards = kExtenderMaxArmLength;
+  public static final double kMaxDownwards = 0;
+  public static final double kHalfway = kExtenderMaxArmLength / 2;
+  public static final double kSlightlyUpward = 6;
 }
