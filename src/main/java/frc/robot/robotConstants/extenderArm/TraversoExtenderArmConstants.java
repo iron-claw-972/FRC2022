2a78e49c3849946f0127da9c5bfb2a9e808b2820
OctraSource <92061729+JusticePeyton@users.noreply.github.com>
2022-02-12 16:23:05 -0800

    proper constants

--- src/main/java/frc/robot/robotConstants/extenderArm/TraversoExtenderArmConstants.java
+++ src/main/java/frc/robot/robotConstants/extenderArm/TraversoExtenderArmConstants.java
@@ -12,12 +12,12 @@ public class TraversoExtenderArmConstants {
 
   // used to convert ticks to inches
   public final double kExtenderTicksPerRotation = 1/2048; // every rotation is 2048 ticks
-  public final double kExtenderGearRatio = 20/1;
-  public final double kExtenderInchesPerRotation = (Math.PI * 6); // 6 pi inches per rotation
+  public final double kExtenderGearRatio = 1/20; // gear ration of 20:1
+  public final double kExtenderInchesPerRotation = (Math.PI * 1) / 1; // 1 pi inches per rotation
 
   // Ticks Per Rotation * Gear Ratio * Inches Per Rotation = Tick Multiple
   // Results as 0 when you calculate this in WPI lib, but it's definitely not 0
-  public final double kExtenderTickMultiple = 0.184077695; //(kExtenderTicksPerRotation * kExtenderGearRatio * kExtenderInchesPerRotation);
+  public final double kExtenderTickMultiple =  0.00007669903; //(kExtenderTicksPerRotation * kExtenderGearRatio * kExtenderInchesPerRotation);
 
   // the arm's length in ticks
   public final double kExtenderMaxArmTicks = kExtenderMaxArmLength / kExtenderTickMultiple;
