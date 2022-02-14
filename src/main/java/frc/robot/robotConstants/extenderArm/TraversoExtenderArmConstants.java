d9bf35bf74bd48084c80490ead53ca547b283d2d
AnthonyFurman <71467649+AnthonyFurman@users.noreply.github.com>
2022-02-14 14:35:29 -0800

    non-static constants

--- src/main/java/frc/robot/robotConstants/extenderArm/TraversoExtenderArmConstants.java
+++ src/main/java/frc/robot/robotConstants/extenderArm/TraversoExtenderArmConstants.java
@@ -3,31 +3,31 @@ package frc.robot.robotConstants.extenderArm;
 public class TraversoExtenderArmConstants {
   // extending motors (for climber)
   // TODO: Change these ports!
-  public static final int kRightExtenderPort = 2;
-  public static final int kLeftExtenderPort = 1;
+  public final int kRightExtenderPort = 2;
+  public final int kLeftExtenderPort = 1;
 
   // the arm's max height - the arm's lowest height (max extension - max compression);
   // TODO: Check the robot's arm length!
-  public static final double kExtenderMaxArmLength = 63 - 38; // at its highest, it's 63 inches, at its lowest its 38 inches
+  public final double kExtenderMaxArmLength = 63 - 38; // at its highest, it's 63 inches, at its lowest its 38 inches
 
   // used to convert ticks to inches
-  public static final double kExtenderTicksPerRotation = 1/2048; // every rotation is 2048 ticks
-  public static final double kExtenderGearRatio = 1/20; // gear ration of 20:1
-  public static final double kExtenderInchesPerRotation = (Math.PI * 1) / 1; // 1 pi inches per rotation
+  public final double kExtenderTicksPerRotation = 1/2048; // every rotation is 2048 ticks
+  public final double kExtenderGearRatio = 1/20; // gear ration of 20:1
+  public final double kExtenderInchesPerRotation = (Math.PI * 1) / 1; // 1 pi inches per rotation
 
   // Ticks Per Rotation * Gear Ratio * Inches Per Rotation = Tick Multiple
   // Results as 0 when you calculate this in WPIlib, but it's definitely not 0
-  public static final double kExtenderTickMultiple =  0.00007669903; //(kExtenderTicksPerRotation * kExtenderGearRatio * kExtenderInchesPerRotation);
+  public final double kExtenderTickMultiple =  0.00007669903; //(kExtenderTicksPerRotation * kExtenderGearRatio * kExtenderInchesPerRotation);
 
   // the arm's length in ticks
-  public static final double kExtenderMaxArmTicks = kExtenderMaxArmLength / kExtenderTickMultiple;
+  public final double kExtenderMaxArmTicks = kExtenderMaxArmLength / kExtenderTickMultiple;
 
   // motor clamps
-  public static final double kMotorClamp = .1;
+  public final double kMotorClamp = .1;
   
   // locations
-  public static final double kMaxUpwards = kExtenderMaxArmLength;
-  public static final double kMaxDownwards = 0;
-  public static final double kHalfway = kExtenderMaxArmLength / 2;
-  public static final double kSlightlyUpward = 6;
+  public final double kMaxUpwards = kExtenderMaxArmLength;
+  public final double kMaxDownwards = 0;
+  public final double kHalfway = kExtenderMaxArmLength / 2;
+  public final double kSlightlyUpward = 6;
 }
