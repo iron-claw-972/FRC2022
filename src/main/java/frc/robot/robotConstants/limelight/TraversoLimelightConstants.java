6a42194df8abe3a8f24e13cf556d6a50c6ac0a18
richtan <29845690+richtan@users.noreply.github.com>
2022-02-16 17:39:31 -0800

    Hub distance method

---
+++ src/main/java/frc/robot/robotConstants/limelight/TraversoLimelightConstants.java
@@ -0,0 +1,7 @@
+package frc.robot.robotConstants.limelight;
+
+import edu.wpi.first.math.util.Units;
+
+public class TraversoLimelightConstants {
+  public final double kTargetHeight = Units.feetToMeters(8) + Units.inchesToMeters(8);
+}
--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,21 +1,26 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.robotConstants.limelight.TraversoLimelightConstants;
 
 public class Limelight extends SubsystemBase {
+  public static TraversoLimelightConstants constants = new TraversoLimelightConstants();
+
   private static Limelight instance;
 
   private NetworkTable m_table;
   private String m_tableName;
   private boolean isConnected = false;
 
-  private double tv;
-  private double tx;
-  private double ty;
-  private double ta;
+  private double tv; // Whether target is detected
+  private double tx; // Horizontal angular displacement of target from crosshair
+  private double ty; // Vertical angular displacement of target from crosshair
+  private double ta; // Target area
+  private double pipeline;
 
   public static Limelight getInstance() {
     if (instance == null) {
@@ -38,32 +43,64 @@ public class Limelight extends SubsystemBase {
     m_table = table;
   }
 
-  public boolean isConnected() {
+  private boolean isConnected() {
     return isConnected;
   }
 
-  public boolean hasValidTarget() {
+  private boolean hasValidTarget() {
     double tv = m_table.getEntry("tv").getDouble(0);
     return (tv == 1.0);
   }
 
-  public double getHorizontalAngularError() {
+  private double getHorizontalAngularOffset() {
     double tx = m_table.getEntry("tx").getDouble(0);
     return tx;
   }
 
-  public double getVerticalAngularError() {
+  private double getVerticalAngularoffset() {
     double ty = m_table.getEntry("ty").getDouble(0);
     return ty;
   }
 
-  public double getTargetArea() {
+  private double getTargetArea() {
     double ta = m_table.getEntry("ta").getDouble(0);
     return ta;
   }
 
-  public double getPipeline() {
+  private double getPipeline() {
+    /*
+      Pipeline 0: Red Cargo
+      Pipeline 1: Blue Cargo
+      Pipeline 2: Upper Hub
+    */
+    return m_table.getEntry("getpipe").getDouble(0);
+  }
+
+  private void setPipeline(double pipeline) {
+    
+  }
+
+  private double getLimelightHeight() {
     return 0.0;
   }
 
+  private double getLimelightAngle(double armAngle) {
+    return 0.0;
+  }
+
+  public double getHubDistance(double armAngle) {
+    return (constants.kTargetHeight - getLimelightHeight()) / (Math.tan(Units.degreesToRadians(getLimelightAngle(armAngle) + getVerticalAngularoffset())));
+  }
+
+  public double getHubOffset() {
+    return 0.0;
+  }
+
+  public double getBallOffset(boolean isRedBall) {
+    return 0.0;
+  }
+
+  public void setLight(boolean on) {
+
+  }
 }
