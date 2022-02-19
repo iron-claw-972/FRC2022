86bb504d062dfa184663c36836f61deaf8361ce2
AnthonyFurman <71467649+AnthonyFurman@users.noreply.github.com>
2022-02-19 11:40:07 -0800

    motor safety still checked

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -56,7 +56,7 @@ public class Robot extends TimedRobot {
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
     m_robotContainer.m_shuffleboard.update();
-    RobotContainer.m_drive.updateMotors();
+    //RobotContainer.m_drive.updateMotors();
      
   }
 
