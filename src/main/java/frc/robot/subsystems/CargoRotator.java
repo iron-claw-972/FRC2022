b5e2192091f195c16416530f515b4e995ce134a4
PandaNinja10 <78180593+PandaNinja10@users.noreply.github.com>
2022-02-20 13:42:21 -0800

    feed forward for cargo rotator added not tested

--- src/main/java/frc/robot/subsystems/CargoRotator.java
+++ src/main/java/frc/robot/subsystems/CargoRotator.java
@@ -6,13 +6,14 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import frc.robot.ControllerFactory;
 import frc.robot.robotConstants.cargoRotator.TraversoCargoRotatorConstants;
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CargoRotator extends SubsystemBase {
-  TraversoCargoRotatorConstants constants = new TraversoCargoRotatorConstants();
+  private TraversoCargoRotatorConstants constants = new TraversoCargoRotatorConstants();
 
   private boolean enabled = false;
   private final DutyCycleEncoder encoder;
@@ -22,6 +23,7 @@ public class CargoRotator extends SubsystemBase {
   private double encoderOffset;
 
   private PIDController armPID = new PIDController(constants.kP , constants.kI , constants.kD);
+  ArmFeedforward feedforward = new ArmFeedforward(constants.kS, constants.kG, constants.kV, constants.kA);
   
   public CargoRotator() {
     encoder = new DutyCycleEncoder(constants.kArmEncoder);
@@ -50,7 +52,8 @@ public class CargoRotator extends SubsystemBase {
       // setpoint = SmartDashboard.getNumber("goal", 0);
 
       // set the arm power according to a PID
-      setOutput(armPID.calculate(currentAngle(), setPoint));
+      // setOutput(armPID.calculate(currentAngle(), setPoint));
+      setVoltage(armPID.calculate(currentAngle(), setPoint) + feedforward.calculate(setPoint*(Math.PI/180), 0));
       SmartDashboard.putNumber("Cargo Arm Angle", currentAngle());
     }
 
@@ -95,6 +98,10 @@ public class CargoRotator extends SubsystemBase {
 
   public void setOutput(double motorPower){
     m_motor.set(ControlMode.PercentOutput, MathUtil.clamp(motorPower, -constants.kMotorClamp, constants.kMotorClamp));
+  }
+
+  public void setVoltage(double motorPower){
+    m_motor.setVoltage(MathUtil.clamp(motorPower, -constants.kMotorClamp*12, constants.kMotorClamp*12));
   }
 
   // sets PID Goal
