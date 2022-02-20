8323cb702bb3e773881c925f8eaeaab5f50fb43c
OctraSource <92061729+JusticePeyton@users.noreply.github.com>
2022-02-20 15:29:58 -0800

    unneeded import

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -11,8 +11,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.robotConstants.drivetrain.*;
 import frc.robot.util.ControllerFactory;
 
-import javax.naming.ldap.ControlFactory;
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
