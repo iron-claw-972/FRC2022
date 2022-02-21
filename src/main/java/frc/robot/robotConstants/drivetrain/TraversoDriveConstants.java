c36e300a1a1feb113290588b830dc8f224570537
Pranav Tadepalli <orca.pranav@gmail.com>
2022-02-21 09:06:18 -0800

    Update TraversoDriveConstants.java

--- src/main/java/frc/robot/robotConstants/drivetrain/TraversoDriveConstants.java
+++ src/main/java/frc/robot/robotConstants/drivetrain/TraversoDriveConstants.java
@@ -47,7 +47,7 @@ public class TraversoDriveConstants {
   public final boolean kLeftEncoderReversed = true;
 
 
-  public final double kSupplyCurrentLimit = 50;
+  public final double kSupplyCurrentLimit = 40;
   public final double kSupplyTriggerThreshold = 50;
   public final double kSupplyTriggerDuration = 0.3;
   public final boolean kCoast = false;
