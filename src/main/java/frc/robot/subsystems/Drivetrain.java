10d4aada0175a4e1c8cad928e93c1f7c9d5dc830
Pranav Tadepalli <orca.pranav@gmail.com>
2022-02-21 09:08:06 -0800

    Update Drivetrain.java

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -118,8 +118,8 @@ public class Drivetrain extends SubsystemBase {
       m_rightMotors = new PhoenixMotorControllerGroup(m_rightMotor1);
     }
 */
-    m_leftMotors = new PhoenixMotorControllerGroup(m_leftMotor1, new WPI_TalonFX(constants.leftMotorPorts[1]));
-    m_rightMotors = new PhoenixMotorControllerGroup(m_rightMotor1, new WPI_TalonFX(constants.rightMotorPorts[1]));
+    m_leftMotors = new PhoenixMotorControllerGroup(m_leftMotor1, ControllerFactory.createTalonFX(constants.leftMotorPorts[1], constants.kSupplyCurrentLimit, constants.kSupplyTriggerThreshold, constants.kSupplyTriggerDuration, constants.kCoast));
+    m_rightMotors = new PhoenixMotorControllerGroup(m_rightMotor1, ControllerFactory.createTalonFX(constants.rightMotorPorts[1], constants.kSupplyCurrentLimit, constants.kSupplyTriggerThreshold, constants.kSupplyTriggerDuration, constants.kCoast));
 
     m_dDrive = new DifferentialDrive(m_leftMotors, m_rightMotors);
 
