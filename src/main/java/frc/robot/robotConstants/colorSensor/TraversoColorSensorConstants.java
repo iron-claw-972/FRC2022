81526c2d3375bb7916a3796fb075d6baef89113b
richtan <29845690+richtan@users.noreply.github.com>
2022-02-23 12:13:39 -0800

    Removed distance sensor and wrote color detection

--- src/main/java/frc/robot/robotConstants/colorSensor/TraversoColorSensorConstants.java
+++ src/main/java/frc/robot/robotConstants/colorSensor/TraversoColorSensorConstants.java
@@ -1,5 +1,8 @@
 package frc.robot.robotConstants.colorSensor;
 
 public class TraversoColorSensorConstants {
-  public final int kMinimumBallProximity = 250;
+  public final double kRedBallThreshold = 0.3;
+  public final double kBlueBallThreshold = 0.3;
+  public final int kEdgeBallProximityThreshold = 110;
+  public final int kSecureBallProximityThreshold = 200;
 }
--- src/main/java/frc/robot/subsystems/BallDetection.java
+++ src/main/java/frc/robot/subsystems/BallDetection.java
@@ -11,65 +11,53 @@ import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 
-import com.revrobotics.*;
-// import com.revrobotics.Rev2mDistanceSensor.Port;
-// import com.revrobotics.Rev2mDistanceSensor.RangeProfile;
-// import com.revrobotics.Rev2mDistanceSensor.Unit;
 
 public class BallDetection extends SubsystemBase {
-/*
+
   TraversoColorSensorConstants constants = new TraversoColorSensorConstants();
 
   private final I2C.Port i2cPort = I2C.Port.kMXP;
   private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
 
+  private Color detectedColor;
+  private int proximity;
   
-  // private Rev2mDistanceSensor distanceSensor = new Rev2mDistanceSensor(Port.kMXP, Unit.kMillimeters, RangeProfile.kHighAccuracy);
-
   public BallDetection(){
+
   }
 
   public void periodic() {
+    detectedColor = m_colorSensor.getColor();
+    proximity = m_colorSensor.getProximity();
 
-    // distanceSensor.setEnabled(true);
-    // distanceSensor.setAutomaticMode(true);
-    // SmartDashboard.putNumber("distance", distanceSensor.getRange());
-    // System.out.println(distanceSensor.getRange());
+    boolean hasRedBall = hasRedBall();
+    boolean hasBlueBall = hasBlueBall();
     
-    // distanceSensor.
-    // SmartDashboard.putBoolean("blue", Color.kBlue == ballColor());
-    // SmartDashboard.putBoolean("red", Color.kRed == ballColor());
-    // SmartDashboard.putBoolean("has ball", containsBall());
-
-
-    SmartDashboard.putNumber("proximity",m_colorSensor.getProximity());
-    System.out.println(m_colorSensor.getProximity());
-
+    SmartDashboard.putBoolean("Has Red Ball", hasRedBall);
+    SmartDashboard.putBoolean("Has Blue Ball", hasBlueBall);
   }
 
-
-  public Color isColor(double red, double green, double blue) {
-    if (blue > 0.4) {
-      return Color.kBlue;
-    } else if (red > 0.4) {
-      return Color.kRed;
-    }
-    return null;
+  public boolean hasRedBall() {
+    return containsBall() && detectedColor.red >= constants.kRedBallThreshold;
   }
 
-  public Color ballColor() {
-    Color detectedColor = m_colorSensor.getColor();
-    return isColor(detectedColor.red, detectedColor.green, detectedColor.blue);
+  public boolean hasBlueBall() {
+    return containsBallSecurely() && detectedColor.blue >= constants.kBlueBallThreshold;
+  }
+
+  public boolean hasRedBallSecure() {
+    return containsBall() && detectedColor.red >= constants.kRedBallThreshold;
+  }
+
+  public boolean hasBlueBallSecure() {
+    return containsBall() && detectedColor.blue >= constants.kBlueBallThreshold;
   }
 
   public boolean containsBall() {
-    int ballProximity = m_colorSensor.getProximity();
-    return (ballProximity > constants.kMinimumBallProximity);
+    return (proximity >= constants.kEdgeBallProximityThreshold);
   }
 
-  public boolean ballShot() {
-    return !containsBall();
+  public boolean containsBallSecurely() {
+    return (proximity >= constants.kSecureBallProximityThreshold);
   }
-
-*/
 }
