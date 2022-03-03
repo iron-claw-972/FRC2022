65599ccee1dc04f712fa17f5110b5bfc09eb5627
AnthonyFurman <71467649+AnthonyFurman@users.noreply.github.com>
2022-03-02 19:49:44 -0800

    add (commented out) non-WPI extender pid tolerance

--- src/main/java/frc/robot/subsystems/ClimbExtender.java
+++ src/main/java/frc/robot/subsystems/ClimbExtender.java
@@ -61,6 +61,7 @@ public class ClimbExtender extends SubsystemBase {
   public boolean reachedSetpoint() {
     // if the current tick position is within the setpoint's range (setpoint +- 10), return true, otherwise return false
     return extenderPID.atSetpoint();
+    //return currentExtension()  < setpoint + constants.kExtenderTolerance && currentExtension()  > setpoint - constants.kExtenderTolerance;
   }
 
   // called in RobotContainer by button binds
