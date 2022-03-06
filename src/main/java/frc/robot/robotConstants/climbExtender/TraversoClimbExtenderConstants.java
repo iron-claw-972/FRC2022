0645cd0b26084ff6f2fe02f379726e30ee5bbf34
Pranav Tadepalli <orca.pranav@gmail.com>
2022-03-06 11:29:37 -0800

    adjust pid

--- src/main/java/frc/robot/robotConstants/climbExtender/TraversoClimbExtenderConstants.java
+++ src/main/java/frc/robot/robotConstants/climbExtender/TraversoClimbExtenderConstants.java
@@ -12,8 +12,8 @@ public class TraversoClimbExtenderConstants {
   public final double kExtenderTolerance = 2000;
 
   // motor clamps
-  public final double kMotorClampDown = -0.8; //should be negative!
-  public final double kMotorClampUp = .6;     //should be positive!
+  public final double kMotorClampDown = -0.9; //should be negative!
+  public final double kMotorClampUp = .7;     //should be positive!
   
   // locations
   public final double kMaxUpwards = kExtenderMaxArmTicks;
@@ -22,9 +22,9 @@ public class TraversoClimbExtenderConstants {
   public final double kSlightlyUpward = kExtenderMaxArmTicks / 6;
   
   // off load PID constants
-  public final double kP = 0.07;
+  public final double kP = 0.08;
   public final double kI = 0.00;
-  public final double kD = 0.00;
+  public final double kD = 0.02;
 
   public final double kSupplyCurrentLimit = 40;
   public final double kSupplyTriggerThreshold = 40;
