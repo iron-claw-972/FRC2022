c25cc343331f20bf9cb88246b5f1f53cfa69c03e
OctraSource <92061729+JusticePeyton@users.noreply.github.com>
2022-03-16 17:17:59 -0700

    replace kMaxDownwards with ExtendDownwards

--- src/main/java/frc/robot/controls/ClimbOperator.java
+++ src/main/java/frc/robot/controls/ClimbOperator.java
@@ -4,10 +4,13 @@ import controllers.GameController;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.RobotContainer;
 import frc.robot.Constants.JoyConstants;
 import frc.robot.commands.ClimbExtenderMove;
 import frc.robot.commands.ClimbRotatorMove;
+import frc.robot.commands.ExtendDownwards;
 import frc.robot.commands.ClimbMove;
 import frc.robot.commands.PositionArm;
 import frc.robot.robotConstants.cargoRotator.TraversoCargoRotatorConstants;
@@ -51,7 +54,7 @@ public class ClimbOperator {
     controller.getDPad().down().whenHeld(new ParallelCommandGroup(
       // stow the cargo subsystem
       new PositionArm(cargoConstants.kStowPos),
-      new ClimbExtenderMove(extend.kMaxDownwards)
+      new ExtendDownwards()
     ));
 
     // when DPad Right is pressed, enable the rotator and go to kMaxForward degrees
@@ -91,9 +94,9 @@ public class ClimbOperator {
     ));
 
     controller.getDPad().down().whenPressed(new SequentialCommandGroup(    
-
+      new ClimbRotatorMove(rotate.kNinetyDeg),
       // when it reaches 90 degrees, compress
-      new ClimbExtenderMove(extend.kMaxDownwards),
+      new ExtendDownwards(),
       
       // after the static hooks are on, extend slightly upwards
       new ClimbExtenderMove(extend.kSlightlyUpward)
@@ -103,13 +106,17 @@ public class ClimbOperator {
       // extend fully and rotate backwards fully
       // rotator should theoretically be faster than the extender
       new ClimbRotatorMove(rotate.kMaxBackward),
+      new PrintCommand("passed climb rotator move"),
       new ClimbExtenderMove(extend.kMaxUpwards),
 
       // after we fully extend and rotate, rotate to the bar
       new ClimbRotatorMove(rotate.kToBar),
 
       // compress fully and rotate to 90 degrees
-      new ClimbMove(extend.kMaxDownwards, rotate.kNinetyDeg),
+      new ParallelCommandGroup(
+        new ExtendDownwards(),
+        new ClimbRotatorMove(rotate.kNinetyDeg)
+      ),
 
       new ClimbExtenderMove(extend.kSlightlyUpward)
     ));
