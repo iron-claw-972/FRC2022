509dbd09e11d0f9b031377f572560957c46e57f3
richtan <29845690+richtan@users.noreply.github.com>
2022-03-20 10:18:52 -0700

    Merge auto-commands

--- src/main/java/frc/robot/commands/Back1BallAuto.java
+++ src/main/java/frc/robot/commands/Back1BallAuto.java
@@ -3,16 +3,19 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
-import frc.robot.robotConstants.cargoRotator.TraversoCargoRotatorConstants;
-import frc.robot.robotConstants.shooterBelt.TraversoBeltConstants;
-import frc.robot.robotConstants.shooterWheel.TraversoCargoShooterConstants;
+import frc.robot.commands.autoCommands.DriveDistance;
+import frc.robot.commands.autoCommands.ShootAuto;
+import frc.robot.commands.cargoCommands.PositionArm;
+import frc.robot.robotConstants.cargoRotator.MarinusCargoRotatorConstants;
+import frc.robot.robotConstants.shooterBelt.MarinusBeltConstants;
+import frc.robot.robotConstants.shooterWheel.MarinusCargoShooterConstants;
 
 public class Back1BallAuto extends SequentialCommandGroup {
   //THIS AUTO IS NOT TESTED AND MAY BE INACCURATE
 
-  public static TraversoCargoRotatorConstants cargoConstants = new TraversoCargoRotatorConstants();
-  public static TraversoBeltConstants beltConstants = new TraversoBeltConstants();
-  public static TraversoCargoShooterConstants wheelConstants = new TraversoCargoShooterConstants();
+  public static MarinusCargoRotatorConstants cargoConstants = new MarinusCargoRotatorConstants();
+  public static MarinusBeltConstants beltConstants = new MarinusBeltConstants();
+  public static MarinusCargoShooterConstants wheelConstants = new MarinusCargoShooterConstants();
 
   public Back1BallAuto() {
     addRequirements(RobotContainer.m_drive, RobotContainer.m_cargoBelt, RobotContainer.m_cargoRotator, RobotContainer.m_cargoShooter);
