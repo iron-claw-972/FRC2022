1ea2ca460e210c637645c73f387436e2998e833b
richtan <29845690+richtan@users.noreply.github.com>
2022-03-20 09:23:21 -0700

    Remove initial wheel spinup for shooting

--- src/main/java/frc/robot/commands/cargoCommands/Shoot.java
+++ src/main/java/frc/robot/commands/cargoCommands/Shoot.java
@@ -24,9 +24,6 @@ public class Shoot extends SequentialCommandGroup {
     ) {
       addRequirements(RobotContainer.m_cargoShooter, RobotContainer.m_cargoRotator, RobotContainer.m_cargoBelt, RobotContainer.m_limelight);
       addCommands(
-        // Start spinning up wheels so PID has less to do later
-        new InstantCommand(() -> ShooterMethods.setWheelRPM(2000)),
-
         new InstantCommand(() -> ShooterMethods.enableAll()),
 
         parallel(
