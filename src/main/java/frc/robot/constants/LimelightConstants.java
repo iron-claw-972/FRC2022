d3a86654baaebf965b4d0b873edc9959276b51c7
richtan <29845690+richtan@users.noreply.github.com>
2022-04-02 15:54:03 -0700

    Lower P for ball chase

--- src/main/java/frc/robot/constants/LimelightConstants.java
+++ src/main/java/frc/robot/constants/LimelightConstants.java
@@ -14,7 +14,7 @@ public class LimelightConstants {
   public final double kAutoThrottlePow = -0.3;
 
   public final double kTurnPIDTolerance = 1;
-  public final double kTurnP = 0.012; // Originally 0.025
+  public final double kTurnP = 0.01; // Originally 0.025
   public final double kTurnI = 0;
   public final double kTurnD = 0;
   public final double kMaxTurnPower = 0.6;
