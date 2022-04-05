6b1742164b0c59bfd07f63ced2ec14553cb1517c
AnthonyFurman <fura5196@lgsstudent.org>
2022-04-05 16:58:33 -0700

    Untested Four Ball

--- src/main/java/frc/robot/commands/auto/routines/Tar2FourBall.java
+++ src/main/java/frc/robot/commands/auto/routines/Tar2FourBall.java
@@ -12,9 +12,8 @@ import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Limelight;
 import frc.robot.commands.auto.PathweaverCommand;
+import frc.robot.commands.auto.PathweaverIntake;
 import frc.robot.commands.auto.ShootAuto;
-import frc.robot.commands.cargo.AlignToUpperHub;
-import frc.robot.commands.cargo.ChaseBall;
 import frc.robot.commands.cargo.PositionArm;
 import frc.robot.util.BallPositions;
 import frc.robot.util.CargoUtil;
@@ -27,52 +26,26 @@ public class Tar2FourBall extends SequentialCommandGroup {
   public Tar2FourBall(Drivetrain drive, Belt belt, Arm arm, Shooter shooter, Limelight limelight, BallDetection ballDetection, Alliance color) {
     addRequirements(drive, belt, arm, shooter, limelight, ballDetection);
     addCommands(
-        new InstantCommand(() -> drive.resetOdometry(BallPositions.getBall(3, color).getRobotPoseFromBall())),
-        new InstantCommand(() -> CargoUtil.setBeltPower(0)),
-        new PathweaverCommand("4ballzero", drive),
-        new ShootAuto(false, false, 1, () -> true, 157, 24),
-        new PositionArm(Constants.arm.kIntakePos),
-        new ParallelDeadlineGroup(
-          new WaitUntilCommand(() -> CargoUtil.isBallContained()).withTimeout(2),
-          new PathweaverCommand("4ballone", drive),
-          sequence(
-            new InstantCommand(() -> CargoUtil.setWheelRPM(Constants.shooter.kIntakeSpeed)),
-            new InstantCommand(() -> CargoUtil.setBeltPower(Constants.belt.kIntakeSpeed)),
-            new InstantCommand(() -> CargoUtil.enableAll())
-          )
-        ),
-        new ShootAuto(false, false, 0, () -> true, 157, 25),
-        parallel(
-          sequence(
-            new PathweaverCommand("4balltwo", drive),
-            new ChaseBall(false, false)
-          ),
-          new PositionArm(Constants.arm.kIntakePos),
-          sequence(
-            new InstantCommand(() -> CargoUtil.setWheelRPM(Constants.shooter.kIntakeSpeed)),
-            new InstantCommand(() -> CargoUtil.setBeltPower(Constants.belt.kIntakeSpeed)),
-            new InstantCommand(() -> CargoUtil.enableAll())
-          )
-        ),
-        new PositionArm(154),
-        new AlignToUpperHub(),
-        new ShootAuto(false, false, 0, () -> true, 157, 25),
-        parallel(
-          sequence(
-            new PathweaverCommand("4ballthree", drive),
-            new ChaseBall(false, false)
-          ),
-          new PositionArm(Constants.arm.kIntakePos),
-          sequence(
-            new InstantCommand(() -> CargoUtil.setWheelRPM(Constants.shooter.kIntakeSpeed)),
-            new InstantCommand(() -> CargoUtil.setBeltPower(Constants.belt.kIntakeSpeed)),
-            new InstantCommand(() -> CargoUtil.enableAll())
-          )
-        ),
-        new PathweaverCommand("4ballfour", drive),
-        new AlignToUpperHub(),
-        new ShootAuto(false, false, 0, () -> true, 157, 25),
-        new PositionArm(154)
+      new InstantCommand(() -> drive.resetOdometry(BallPositions.getBall(3, color).getRobotPoseFromBall())),
+      new InstantCommand(() -> CargoUtil.setBeltPower(0)),
+      new PathweaverCommand("4ballzero", drive),
+      new ShootAuto(false, false, 1, () -> true, 157, 24),
+
+      new PositionArm(Constants.arm.kIntakePos),
+      new PathweaverIntake("4ballone"),
+
+      new ShootAuto(false, false, 0, () -> true, 157, 25),
+
+      new PathweaverIntake("4balltwo"),
+
+      new ShootAuto(false, false, 0, () -> true, 157, 25),
+      
+      new PathweaverIntake("4ballthree"),
+
+      new PathweaverCommand("4ballfour", drive),
+      new ShootAuto(false, false, 0, () -> true, 157, 25),
+
+      new PositionArm(154)
     );
   }
 }
