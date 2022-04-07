565d59fb4fa48acbc0573dc53ad436aedfa1b3b9
Lakshya <lakshag32@gmail.com>
2022-04-07 16:54:52 -0700

    very basic unit testing, more like practice for me. Tests if setPower() and setStop() set m_motorPower to the write value

--- src/main/java/frc/robot/commands/auto/routines/ThreeBallDefenseOne.java
+++ src/main/java/frc/robot/commands/auto/routines/ThreeBallDefenseOne.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.auto.routines;
 
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Robot;
@@ -7,18 +8,38 @@ import frc.robot.commands.auto.PathweaverCommand;
 import frc.robot.commands.auto.PathweaverIntake;
 import frc.robot.commands.auto.ShootAuto;
 import frc.robot.constants.Constants;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Belt;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Limelight;
+import frc.robot.subsystems.Shooter;
 import frc.robot.util.BallPositions;
 
 public class ThreeBallDefenseOne extends SequentialCommandGroup {
-    public ThreeBallDefenseOne() {
-        addCommands(
-            new TwoBallPW(),
 
-            new PathweaverIntake("4balltwo", false, true),
-    
-            new ShootAuto(false, true, 0, () -> true, 109, 26)
 
-            //TODO: add defense ball
-        );
-    }
+  public ThreeBallDefenseOne(Alliance color) {
+    this(Robot.drive, Robot.belt, Robot.arm, Robot.shooter, Robot.ll, color);
+  }
+
+  public ThreeBallDefenseOne(Drivetrain drive, Belt belt, Arm arm, Shooter shooter, Limelight ll, Alliance color) {
+    addRequirements(drive, belt, arm, shooter, ll);
+    addCommands(
+      new InstantCommand(() -> drive.resetOdometry(BallPositions.B1.getRobotPoseFromBall())),
+
+      new PathweaverIntake("3BallTurn_0", false, true),
+      new ShootAuto(false, true, 0, () -> false, 153, 25),
+      new PathweaverCommand("3BallTurn_1", false, false),
+      new PathweaverIntake("3BallTurn_2", false, true),
+      new ShootAuto(false, true, 0, () -> false, 153, 25),
+      new PathweaverIntake("3BallTurn_3", false, false),
+      new ShootAuto(false, true, 0, () -> false, 153, 25),
+      new PathweaverCommand("3BallTurn_4", false, false)
+    );
+  
+
+}
+
+      
+  
 }
