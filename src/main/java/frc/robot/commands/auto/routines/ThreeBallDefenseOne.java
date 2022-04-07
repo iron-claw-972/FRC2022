b2022b744e657a3a727d99e2f140e7f9b556e6e6
nicshima <90671126+nicshima@users.noreply.github.com>
2022-04-06 18:02:36 -0700

    fix imports

--- src/main/java/frc/robot/commands/auto/routines/ThreeBallDefenseOne.java
+++ src/main/java/frc/robot/commands/auto/routines/ThreeBallDefenseOne.java
@@ -1,16 +1,19 @@
 package frc.robot.commands.auto.routines;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Robot;
 import frc.robot.commands.auto.PathweaverCommand;
 import frc.robot.commands.auto.PathweaverIntake;
 import frc.robot.commands.auto.ShootAuto;
 import frc.robot.constants.Constants;
+import frc.robot.util.BallPositions;
 
 public class ThreeBallDefenseOne extends SequentialCommandGroup {
     public ThreeBallDefenseOne() {
         addCommands(
             
-            new InstantCommand(() -> drive.resetOdometry(BallPositions.B3.getRobotPoseFromBall())),
+            new InstantCommand(() -> Robot.drive.resetOdometry(BallPositions.B3.getRobotPoseFromBall())),
 
             new PathweaverCommand("4balldefenseone", false, true),
             new PathweaverIntake("4", true),
